d496ae525d34028486030cb1ea7b2ec492a4a1ba
Zig1375 <zig@zig.zag>
2016-05-31 11:23:28 +0300

    fix

--- shim.h
+++ shim.h
@@ -5,55 +5,17 @@
 
 #define CURL_TRUE  1
 #define CURL_FALSE 0
-/*
-extern inline EmaillHelpers_send(char *HOST, char *FROM, char *TO ) {
-    CURL *curl;
-    CURLcode res = CURLE_OK;
-    struct curl_slist *recipients = NULL;
-    struct upload_status upload_ctx;
 
-    upload_ctx.lines_read = 0;
-
-    curl = curl_easy_init();
-
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_USERNAME, "user");
-        curl_easy_setopt(curl, CURLOPT_PASSWORD, "secret");
-        curl_easy_setopt(curl, CURLOPT_URL, HOST);
-        curl_easy_setopt(curl, CURLOPT_USE_SSL, (long)CURLUSESSL_ALL);
-        curl_easy_setopt(curl, CURLOPT_MAIL_FROM, FROM);
-
-        recipients = curl_slist_append(recipients, TO);
-        curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
-
-        curl_easy_setopt(curl, CURLOPT_READFUNCTION, payload_source);
-        curl_easy_setopt(curl, CURLOPT_READDATA, &upload_ctx);
-        curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-
-        res = curl_easy_perform(curl);
-
-        if(res != CURLE_OK)
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-
-        curl_slist_free_all(recipients);
-        curl_easy_cleanup(curl);
-    }
-
-    return (int)res;
+extern inline CURLcode curlHelperSetOptUseSSL(CURL *curl) {
+    return curl_easy_setopt(curl, CURLOPT_USE_SSL, (long)CURLUSESSL_TRY);
 }
-
- */
-
-
 
 extern inline CURLcode curlHelperSetOptBool(CURL *curl, CURLoption option, int yesNo) {
     return curl_easy_setopt(curl, option, yesNo == CURL_TRUE ? 1L : 0L);
 }
 
-extern inline CURLcode curlHelperSetOptHeaders(CURL *curl, struct curl_slist *headers) {
-    return curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+extern inline CURLcode curlHelperSetOptHeaders(CURL *curl, CURLoption option, struct curl_slist *headers) {
+    return curl_easy_setopt(curl, option, headers);
 }
 
 extern inline CURLcode curlHelperSetOptInt(CURL *curl, CURLoption option, long data) {
@@ -64,26 +26,12 @@ extern inline CURLcode curlHelperSetOptString(CURL *curl, CURLoption option, cha
     return curl_easy_setopt(curl, option, data);
 }
 
-extern inline CURLcode curlHelperSetOptReadFunc(CURL *curl, void *userData, size_t (*read_cb) (char *buffer, size_t size, size_t nitems, void *userdata)) {
-
-    CURLcode rc = curl_easy_setopt(curl, CURLOPT_READDATA, userData);
-    if  (rc == CURLE_OK) {
-        rc = curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_cb);
-    }
-    return rc;
-
+extern inline CURLcode curlHelperSetOptReadFunc(CURL *curl, size_t (*read_cb) (char *buffer, size_t size, size_t nitems, void *userdata)) {
+    return curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_cb);
 }
 
-extern inline CURLcode curlHelperSetOptWriteFunc(CURL *curl, void *userData, size_t (*write_cb) (char *ptr, size_t size, size_t nmemb, void *userdata)) {
-
-    CURLcode rc = curl_easy_setopt(curl, CURLOPT_HEADER, 1);
-    if  (rc == CURLE_OK)  {
-        rc = curl_easy_setopt(curl, CURLOPT_WRITEDATA, userData);
-        if  (rc == CURLE_OK) {
-            rc = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
-        }
-    }
-    return rc;
+extern inline CURLcode curlHelperSetOptReadData(CURL *curl, void *userData) {
+    return curl_easy_setopt(curl, CURLOPT_READDATA, userData);
 }
 
 extern inline CURLcode curlHelperGetInfoCString(CURL *curl, CURLINFO info, char **data) {
